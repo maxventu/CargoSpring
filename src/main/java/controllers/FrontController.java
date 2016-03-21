22a11902a44fb9ec5d70b574afde787e7a881087
maxventu <1995_maxim@mail.ru>
2016-03-21 19:59:41 +0300

    app is running now

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -1,7 +1,30 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+
 /**
  * Created by maksim.kalenik on 18.03.2016.
  */
+@Controller
 public class FrontController {
+    @PostConstruct
+    public void init() {
+    }
+
+    @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
+    public ModelAndView showStartPage(Model model) {
+        ModelAndView modelAndView =  new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+    @RequestMapping(value = {"/", "index"}, method = RequestMethod.POST)
+    public String showPageAfterLogout() {
+        return "index";
+    }
 }
