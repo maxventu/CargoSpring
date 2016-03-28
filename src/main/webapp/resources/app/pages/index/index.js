1e63a62fd227569c3e5f6aafc9f2ae1399895e83
maxventu <1995_maxim@mail.ru>
2016-03-28 20:58:17 +0300

    v0.1.1 start of work on spring security

--- src/main/webapp/resources/app/pages/index/index.js
+++ src/main/webapp/resources/app/pages/index/index.js
@@ -1,6 +1,5 @@
 (function() {
     angular.module("CargoSpring")
         .controller('indexController', ['$scope', function ($rootScope,$scope) {
-            console.log("index js init");
         }]);
 })();
